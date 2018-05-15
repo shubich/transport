658a3ed74c800b07ac8464c51d83425bdebf90b1
shubich <shaa5@yahoo.com>
2018-05-16 00:22:09 +0300

    Move users to table

--- client/src/pages/Admin/subPages/Users/Users.vue
+++ client/src/pages/Admin/subPages/Users/Users.vue
@@ -1,6 +1,28 @@
 <template>
   <div class="wrapper">
-    <pre>{{users}}</pre>
+    <table>
+      <thead>
+        <tr>
+          <th>Номер карты</th>
+          <th>Имя пользователя</th>
+          <th>Роль</th>
+          <th>Статус</th>
+          <th>Счёт</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="item in users"
+          :key="item._id"
+        >
+          <td>{{item.cardNumber}}</td>
+          <td>{{item.loginName}}</td>
+          <td>{{item.role}}</td>
+          <td>{{item.active ? "Active" : "Blocked"}}</td>
+          <td>{{item.bill}}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
